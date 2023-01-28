95fc06149ab065df1cb9e2f496cb18e895b23071
Adam Dubey <adubey0311@gmail.com>
2023-01-27 21:02:44 -0500

    [AD] Refactoring frontend to use Axios for API integration

--- frontend/scripts/visitorCount.js
+++ frontend/scripts/visitorCount.js
@@ -1,13 +1,9 @@
 "use strict";
 
-$(document).ready(() => {
-    $.post('https://k1ywrf8h8e.execute-api.us-east-1.amazonaws.com/Prod/visit')
-    .done(visitor_counter => {
-        $('#loader').hide();
-        $('#visits').text(visitor_counter);
+axios.post("https://k1ywrf8h8e.execute-api.us-east-1.amazonaws.com/Prod/visit")
+    .then(function (response) {
+    document.getElementById("visits").innerHTML = response.data;
     })
-    .fail(e => {
-        console.log('Error: ');
-        console.log(e);
+    .catch(function (error) {
+    console.log(error);
     });
-});
